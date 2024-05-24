9433dc192dc427d6d4f503afdddb05d7390da7c6
AunikIslam <aunikislam172@gmail.com>
2024-05-24 15:22:09 +0600

    Initial Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
-
 const routes: Routes = [
   {
-    path: 'task-add',
+    path: 'add-task',
     loadChildren: () =>
       import('./main/add-task/add-task.module').then((m) => m.AddTaskModule),
   }
